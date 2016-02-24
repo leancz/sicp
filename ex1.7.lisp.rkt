603a89d1659f9b31ebba82b487c0ab9107b87f96
Paul Baines <paulbaines@hotmail.com>
2016-02-24 13:17:50 +0100

    Fixes after http://codereview.stackexchange.com/questions/120625/square-root-calculation-in-scheme-sicp-exercise-1-7

--- ex1.7.lisp.rkt
+++ ex1.7.lisp.rkt
@@ -2,9 +2,9 @@
 (define (average . ns) (/ (apply + ns) (length ns)))
 
 (define (change-in-precision guess x)
-  ( - (- guess (average guess (/ x guess)))))
+  (- (average guess (/ x guess)) guess ))
 
 (define (sqrt guess x)
   (if (< (abs (change-in-precision guess x)) (/ 0.00000001 guess))
-  (+ guess (change-in-precision guess x))
-  (sqrt (+ guess (change-in-precision guess x)) x)))
+      guess
+      (sqrt (+ guess (change-in-precision guess x)) x)))
