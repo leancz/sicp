038c4efcd928b31313d65ede93e6bf76acf8a131
Paul Baines <paulbaines@hotmail.com>
2016-02-25 12:37:19 +0100

    Move sub-functions into the scope of sqrt

--- ex1.7.lisp.rkt
+++ ex1.7.lisp.rkt
@@ -1,11 +1,9 @@
 #lang racket
-(define (average . ns) (/ (apply + ns) (length ns)))
-
-(define (change-in-precision guess x)
-  (- (average guess (/ x guess)) guess ))
-
 
 (define (sqrt guess x)
+  (define (average . ns) (/ (apply + ns) (length ns)))
+  (define (change-in-precision guess x)
+    (- (average guess (/ x guess)) guess ))
   (let ([delta (change-in-precision guess x)])
     (if (< (abs delta) (/ 0.000000001 guess))
         guess
